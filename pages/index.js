196708ce463db378fd7aadfced4549a1ebbf7c34
Tom Lou <mariolzx@gmail.com>
2022-03-22 15:48:27 -0400

    final version

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({collectionId, collectionName, collectionViewUrl, collectionPrice, artworkUrl60, releaseDate}) => (
+            <li className={utilStyles.listItem} key={collectionId}>
+              <Link href={`/posts/${collectionId}`}>
+                <a>{collectionName}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={releaseDate} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = await getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
